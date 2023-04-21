effc3a674edeb1195d88367d9b08f403d8f49bf1
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-04-21 19:38:33 +0200

    goodAction

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -13,14 +13,46 @@ class ViewController: UIViewController {
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
     @IBOutlet var startButton: UIButton!
+    var lightPosion = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startButton.layer.cornerRadius = 10
+        redView.layer.cornerRadius = 90
+        yellowView.layer.cornerRadius = 90
+        greenView.layer.cornerRadius = 90
     }
-
+    
     @IBAction func pushStartButton() {
+        lightPosion += 1
+        
+        startButton.setTitle(
+            lightPosion == 0  ? "Start" : "Next",
+            for: .normal)
+        
+        switch lightPosion{
+        case 0:
+            self.redView.alpha = 0.3
+            self.yellowView.alpha = 0.3
+            self.greenView.alpha = 0.3
+        case 1:
+            self.redView.alpha = 1
+            self.yellowView.alpha = 0.3
+            self.greenView.alpha = 0.3
+        case 2:
+            self.redView.alpha = 0.3
+            self.yellowView.alpha = 1
+            self.greenView.alpha = 0.3
+        case 3:
+            self.redView.alpha = 0.3
+            self.yellowView.alpha = 0.3
+            self.greenView.alpha = 1
+            lightPosion = 0
+        default:
+            lightPosion = 0
+        }
+        
+        
     }
     
 }
-
