bbd829419c31ec31c65fc7c4d6b7bfe2c7e05192
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-04-21 19:13:15 +0200

    new element on view

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func pushStartButton() {
+    }
+    
 }
 
